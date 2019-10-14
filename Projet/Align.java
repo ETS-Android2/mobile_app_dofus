cd1fd4be8707402617ec98671af50cf1af85fc42
Sebas-tien <49407521+Sebas-tien@users.noreply.github.com>
2019-10-14 11:45:19 +0200

    Add files via upload

---
+++ Projet/Align.java
@@ -0,0 +1,4 @@
+public enum Align {
+	NEUTRAL, BONTA, BRAKMAR;
+	
+}
---
+++ Projet/Personnage.java
@@ -0,0 +1,61 @@
+package personne;
+
+public class Personnage {
+
+	private int level;
+	private int ID;
+	private enum Classes;
+	private ;
+	private ;
+	private ;
+	private ;
+	private ;
+	private ;
+	private ;
+	
+
+	/**
+	*
+	*/
+	public Personnage(int lvl, int ID) {
+		this.level = lvl;
+		this.ID = ID;
+		
+	}
+	
+	// public Personne(Personne p) {
+		// this(p.nom, p.prenom);
+	// }
+
+	
+	// public String getNom() {
+		// return nom;
+	// }
+	
+	// public void setNom(String nom) {
+		// this.nom = nom;
+	// }
+
+	// /**
+	// * Retourne le prénom de la personne.
+	// * @return le prénom de la personne
+	// */    
+	// public String getPrenom() {
+		// return prenom;
+	// }
+	
+	
+	// public String toString(){
+		// return(nom+" "+prenom);	
+	// }
+	
+	// public Personne clone() throws CloneNotSupportedException { 
+        // Personne ref = (Personne) super.clone();
+		
+		// return ref ; 
+    // } 
+	
+	// public boolean equals(Personne p) {
+		// return ((this.nom.equals(p.nom))&&(this.prenom.equals(p.prenom)));
+	// }
+}
