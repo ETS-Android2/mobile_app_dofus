1a19d61991f06ada83d4c1c7671a16d4a0eb513d
Angom8 <angom8@outlook.fr>
2019-10-14 14:16:57 +0200

    Update Item.java

--- Projet/Item.java
+++ Projet/Item.java
@@ -1,10 +1,21 @@
 public class Item{
 
-	public final static
+	public final static NBPARAM = 50;
+	
 	private ItemType type;
+	
+	/*Pre-requis*/
 	private int [] required;
+	
+	/*Caracs*/
 	private int [] stats;
+	
+	/*Forgemagie*/
+	private int [] fm;
+	
 	private Panoplie panoplie;
+	
+	/* = Celui de la BDD*/
 	private int ID;
 	private String desc;
 	private int niveau;
@@ -14,22 +25,21 @@ public class Item{
 		type = ItemType.DOFUS;
 		required = new int[50];
 		stats = new int[50];
+		fm = new int[50];
 		panoplie = null;
 		ID = -1;
 		desc = "Default Item";
 
 	}
 
-	/*item custom*/
-	public Item(ItemType type, ){
+	public Item(ItemType type, int ID){
 
-		type = ItemType.DOFUS;
-		required = new int[50];
-		panoplie = null;
-		ID = /*regarde la dernière clé de la bdd et ajoute 1*/;
-		desc = "Custom Item";
+		/*GET DE LA BDD les infos via ID*/
+		this.type = type;
 
 	}
+	
+	/*Pour les items custom : Gestion à part de la bdd (local)
 
 	public Item(int ID){
 
@@ -37,11 +47,45 @@ public class Item{
 
 	}
 
+	*/
+
 	public getType(){return this.type;}
+
 	public getCaracs(){
 		retour = new int[50];
 		int i;
-		for(i=0;i<50;
-}
+		for(i=0;i<50;i++){
+			
+			retour[i] = this.stats[i];
+			
+		}
+		return retour;
+	}
 
+	public getCarac(int i){
+		return stats[i];
+	}
+	
+	public getReq(){
+		retour = new int[50];
+		int i;
+		for(i=0;i<50;i++){
+			
+			retour[i] = this.required[i];
+			
+		}
+		return retour;
+	}
+
+
+	public getFm(){
+		retour = new int[50];
+		int i;
+		for(i=0;i<50;i++){
+			
+			retour[i] = this.fm[i];
+			
+		}
+		return retour;
+	}
 }
