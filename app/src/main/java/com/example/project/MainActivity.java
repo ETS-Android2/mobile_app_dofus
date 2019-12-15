31b8e4b7b3948555515d61c5b47a266c1906e5fa
Angom8 <angom8@outlook.fr>
2019-12-15 16:15:02 +0100

    Night Mode

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -11,18 +11,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         Intent loading = new Intent(MainActivity.this, loadingActivity.class);
         startActivity(loading);
 
-        try {
-            Thread.sleep(2000);
-        } catch (Exception e) {
-        }
-
-        Intent mainMenu = new Intent(MainActivity.this, MainMenu.class);
-        startActivity(mainMenu);
 
     }
 
--- app/src/main/java/com/example/project/MainMenu.java
+++ app/src/main/java/com/example/project/MainMenu.java
@@ -3,14 +3,19 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 import com.example.project.ui.login.LoginActivity;
 
@@ -19,17 +24,71 @@ public class MainMenu extends AppCompatActivity {
 
     protected boolean playingMusic = false;
     protected MediaPlayer m;
+    protected Switch night_m;
+    public static final String PREFS_NAME = "MyPrefsFile";
+    protected final int pressTimeout = 5000;
+
+    private final Handler handler = new Handler();
+    private final Runnable runnable = new Runnable() {
+        public void run() {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        int theme = prefs.getInt("theme", 0);
+
+
+        if(theme == 1){
+            setTheme(R.style.DarkAppTheme);
+        }
+        else{
+            setTheme(R.style.AppTheme);
+        }
+
         setContentView(R.layout.activity_main_menu);
+
+        Switch night_m = (Switch) findViewById(R.id.switch1);
+        if(theme == 1){
+            night_m.setChecked(true);
+        }
+        else{
+            night_m.setChecked(false);
+        }
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
         m = MediaPlayer.create(MainMenu.this, R.raw.m);
         imageView1.setOnTouchListener(touchListenerImg);
 
+
+        night_m.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences pref = getApplicationContext().getSharedPreferences(PREFS_NAME, 0); // 0 - for private mode
+                SharedPreferences.Editor editor = pref.edit();
+
+                if (isChecked) {
+                    editor.putInt("theme", 1);
+                    editor.commit();
+                }
+                else {
+                    editor.putInt("theme", 0);
+                    editor.commit();
+                }
+                MainMenu.this.finish();
+
+            }
+        });
+
+    }
+    protected void onStop() {
+        super.onStop();
     }
 
     public void loadNews(View view){
@@ -71,6 +130,8 @@ public class MainMenu extends AppCompatActivity {
         startActivity(loginActivity);
 
     }
+
+
     private View.OnTouchListener touchListenerImg = new View.OnTouchListener() {
 
         final GestureDetector gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
@@ -90,5 +151,6 @@ public class MainMenu extends AppCompatActivity {
         };
 
     };
+
 
 }
