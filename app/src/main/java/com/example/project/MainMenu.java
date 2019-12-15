5d534843fe40592ad052290c1e5dea155ad9042e
Angom8 <angom8@outlook.fr>
2019-12-15 13:30:05 +0100

    Logo + responsivity improvments

--- app/src/main/java/com/example/project/MainMenu.java
+++ app/src/main/java/com/example/project/MainMenu.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.project.ui.login.LoginActivity;
 
 public class MainMenu extends AppCompatActivity {
+
+
+    protected boolean playingMusic = false;
+    protected MediaPlayer m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,10 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+        m = MediaPlayer.create(MainMenu.this, R.raw.m);
+        imageView1.setOnTouchListener(touchListenerImg);
+
     }
 
     public void loadNews(View view){
@@ -58,5 +71,24 @@ public class MainMenu extends AppCompatActivity {
         startActivity(loginActivity);
 
     }
+    private View.OnTouchListener touchListenerImg = new View.OnTouchListener() {
+
+        final GestureDetector gestureDetector = new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
+            public void onLongPress(MotionEvent e) {
+                if(! playingMusic) {
+                    m.start();
+                    playingMusic = true;
+                }
+                else{m.pause();  playingMusic = false;}
+            }
+        });
+
+        public boolean onTouch(View v, MotionEvent event) {
+
+            return gestureDetector.onTouchEvent(event);
+
+        };
+
+    };
 
 }
